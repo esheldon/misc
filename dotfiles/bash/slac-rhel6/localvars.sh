356571b84e2ec0046b45732b9aaa6c692f5c1162
Erin Sheldon <erin.sheldon@gmail.com>
2015-04-08 18:22:16 -0700

    starting slac environment

---
+++ dotfiles/bash/slac-rhel6/localvars.sh
@@ -0,0 +1,35 @@
+#export MAXBCG_CATDIR=/astro/tutti1/esheldon/lensinputs-v1/maxbcg/catalog
+#export CLUSTERS_INPUT=~/oh/clusters-input
+
+#export PHOTOZ_DIR=~esheldon/photoz
+#export SWEEP_REDUCE=~esheldon/sweep-reduce
+
+#export MASK_DIR=~esheldon/masks
+
+#export LENSDIR=~esheldon/lensing
+#export LENSDIR_HDFS=hdfs:///user/esheldon/lensing
+
+#export GMIX_SDSS=~esheldon/gmix-sdss
+
+#export PIXEL_MASK_BASIC=pixel_mask_dr4_basic
+#export PIXEL_MASK_BOUND=pixel_mask_dr4_bound
+#export PIXEL_MASK_SIMPLE=pixel_mask_dr4_simple
+#export PIXEL_MASK_BASIC_PRINCETON=$MASK_DIR/pixel_mask_princeton_basic
+
+# location we keep simulations of the regauss algorithm
+#export REGAUSSIM_DIR=~esheldon/lensing/regauss-sim
+#export REGAUSSIM_HDFS_DIR=hdfs:///user/esheldon/lensing/regauss-sim
+
+#export SGSEP_DIR=~esheldon/oh/star-galaxy-separation/
+
+#export CLUSTERSTEP=~/lensing/cluster-step
+#export CLUSTERSTEP_HDFS=hdfs:///user/esheldon/lensing/cluster-step
+
+#export TMPDIR=/data/esheldon/tmp
+
+#export DESWL_CHECKOUT=~/git/deswl
+
+#export DESREMOTE_RSYNC=desar2.cosmology.illinois.edu::DESFiles
+#export DES_RSYNC_PASSFILE=~/.des_rsync_pass
+
+#export COSMOS_DIR=$LENSDIR/galsim-cosmos-data
---
+++ dotfiles/bash/slac-rhel6/modules.sh
@@ -0,0 +1,3 @@
+module load use.own
+
+module load anaconda/local
