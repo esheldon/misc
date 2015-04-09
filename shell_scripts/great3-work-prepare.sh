977a7a273aff3181fa87bda0a2803968e415d299
Erin Sheldon <erin.sheldon@gmail.com>
2015-04-08 22:19:27 -0400

    added data dirs

--- shell_scripts/great3-work-prepare.sh
+++ shell_scripts/great3-work-prepare.sh
@@ -13,10 +13,16 @@ module unload espy_packages &> /dev/null
 module unload biggles &> /dev/null
 module unload anaconda &> /dev/null
 module unload espy &> /dev/null
+module unload galsim &> /dev/null
 
 module load anaconda/gpfs
 module load espy/work
 
 source activate great3-work
 
+module load great3sims/work
+
 # add great3 datadir etc here
+
+export GREAT3_DATA_DIR=/gpfs01/astro/workarea/esheldon/mygreat3
+export GREAT3_CONFIG_DIR=/astro/u/esheldon/git/great3-config
