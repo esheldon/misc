4c731a5d20e25add419b96856ae4fa467fd85683
Erin Sheldon <erin.sheldon@gmail.com>
2017-03-23 12:05:39 -0400

    getting working on office machine

--- dotfiles/awesome/rc.lua
+++ dotfiles/awesome/rc.lua
@@ -372,8 +372,13 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
-    { rule = { title = "xplot" },
+
+    --title didn't work on my work machine
+    --{ rule = { title = "xplot" },
+    --  properties = { floating = true } },
+    { rule = { name = "xplot" },
       properties = { floating = true } },
+
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
