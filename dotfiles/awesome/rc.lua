294691047de8d9fa3b5fd7dc4c700ca4887af9ea
Erin Sheldon <erin.sheldon@gmail.com>
2017-03-24 15:55:17 -0400

    feh floats

--- dotfiles/awesome/rc.lua
+++ dotfiles/awesome/rc.lua
@@ -379,6 +379,9 @@ awful.rules.rules = {
     { rule = { name = "xplot" },
       properties = { floating = true } },
 
+    { rule = { class = "feh" },
+      properties = { floating = true } },
+
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
