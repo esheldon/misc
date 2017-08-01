0a9e5dca4a299bf8489ffbe21e6b70e6baf472f6
Erin Sheldon <erin.sheldon@gmail.com>
2017-08-01 13:51:18 -0700

    cori modules

---
+++ dotfiles/bash/nersc/modules-cori.sh
@@ -0,0 +1,21 @@
+#module load use.own
+module use $HOME/privatemodules
+
+#
+# global
+#
+
+#
+# this is the nersc anaconda 2.7
+#
+module load python
+
+module load local
+module load espy/work
+
+#
+# installs in my home
+#
+
+module load perllib
+module load shell_scripts
