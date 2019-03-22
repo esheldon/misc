11456f6082c9df417b6c2bb64d78757d91b28973
Erin Sheldon <erin.sheldon@gmail.com>
2019-03-22 15:30:30 -0400

    updates

--- dotfiles/ipython/ipython_config.py
+++ dotfiles/ipython/ipython_config.py
@@ -395,7 +395,7 @@ c.TerminalInteractiveShell.editing_mode = 'vi'
 # 
 # This will enable completion on elements of lists, results of function calls,
 # etc., but can be unsafe because the code is actually evaluated on TAB.
-# c.IPCompleter.greedy = False
+c.IPCompleter.greedy = False
 c.Completer.use_jedi = False
 
 app = c.InteractiveShellApp
