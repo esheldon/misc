3f5f6918437ada0ee6ac111272f965d6e56206c8
Erin Sheldon <erin.sheldon@gmail.com>
2020-02-04 13:27:15 -0600

    added bebop dir

---
+++ dotfiles/bash/bebop/modules.sh
@@ -0,0 +1,16 @@
+module load intel/18.0.4-443hhug mpich/3.3-verbs-at6arvo
+
+module use /home/esheldon/privatemodules
+
+module load local
+module load espy/local
+
+module load anaconda/py3
+module load shell_scripts
+module load perllib
+
+
+# put this hear since we set up the anaconda
+# using modules
+
+source activate mystack
