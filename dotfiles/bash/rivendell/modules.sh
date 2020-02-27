3552f349d23467089716bcb26d967fb3e8ccd386
Erin Sheldon <erin.sheldon@gmail.com>
2020-02-27 13:04:05 -0500

    update

--- dotfiles/bash/rivendell/modules.sh
+++ dotfiles/bash/rivendell/modules.sh
@@ -2,19 +2,10 @@ f=/etc/profile.d/modules.sh
 if [[ -e $f ]]; then
     source "$f"
 
-    # those marked with * have platform dependent code, e.g. the are
-    # stand-alone C or extensions for python, etc.
-
     module load use.own
 
-    # python
-    #module load espy/local
-    #module load fitsio/py3
-    #module load pymangle/py3
-    #module load biggles/python3
-    #module load healpix/local
-
-    module load local      # *
+    module load local
+    module load espy/local
     module load anaconda/3
     module load shell_scripts
 
