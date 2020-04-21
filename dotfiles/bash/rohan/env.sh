ca76677a78de5539407ecdab64c229c33fa9594a
Erin Sheldon <erin.sheldon@gmail.com>
2020-04-21 11:22:06 -0400

    add scripts for rohan

---
+++ dotfiles/bash/rohan/env.sh
@@ -0,0 +1,26 @@
+export TMPDIR=$HOME/data/tmp
+
+export LENSDIR=$HOME/lensing
+
+export DESDATA=$HOME/data/DES
+export DESREMOTE=https://desar2.cosmology.illinois.edu/DESFiles/desardata
+export DESREMOTE_RSYNC=desar2.cosmology.illinois.edu::ALLDESFiles/desarchive
+export DES_RSYNC_PASSFILE=$HOME/.des_rsync_pass
+export DESPROJ=OPS
+
+export SHAPESIM_DIR=$LENSDIR/shapesim
+export NGMIXER_OUTPUT_DIR=$HOME/data/ngmixer-outputs
+
+export MEDS_DIR=$HOME/data/DES/meds
+
+export COSMOS_DIR=$HOME/data/cosmos
+export MEDSCOSMOS_DIR=$HOME/data/cosmos/meds
+export MEDSCOSMOS_CONFIG_DIR=$HOME/data/cosmos/medscosmos-config
+
+export FITCOSMOS_DIR=$HOME/data/cosmos/outputs
+export FITCOSMOS_CONFIG_DIR=$HOME/data/cosmos/fitcosmos-config
+
+export CATSIM_DIR=$HOME/data/catsim
+
+export FITVD_DIR=$HOME/data/fitvd/outputs
+export FITVD_CONFIG_DIR=$HOME/data/fitvd/fitvd-config
